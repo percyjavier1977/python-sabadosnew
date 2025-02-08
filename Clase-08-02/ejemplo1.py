2ad737b18b34da096719ef9f1578d5e77bff4cea
percyjavier1977 <javierizquierdopajuelo@gmail.com>
2025-02-08 18:51:53 -0500

    Python-08-02

---
+++ Clase-08-02/ejemplo1.py
@@ -0,0 +1,24 @@
+#Operadores de asignación
+x = 5
+print(x)
+#x = x  + 3 
+x += 3
+print(x)
+
+y = 5
+
+#y = y - 2
+y -= 2
+print(y)
+
+
+z = 5
+#z = z * 2
+z *= 2
+print(z)
+
+a = 10
+#a = a / 2
+
+a /= 2
+print(a)
